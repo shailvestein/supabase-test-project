67970360fdb9983b78afafbd1fffedd6f417626c
shailvestein <62176093+shailvestein@users.noreply.github.com>
2025-05-19 21:51:19 +0530

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -4,8 +4,10 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv('SUPABASE_URL')
-SUPABASE_ANNON_KEY = os.getenv('SUPABASE_ANNON_KEY') 
+# SUPABASE_URL = os.getenv('SUPABASE_URL')
+# SUPABASE_ANNON_KEY = os.getenv('SUPABASE_ANNON_KEY') 
+SUPABASE_URL = "https://hlndxtvvcpxvwmacfjhp.supabase.co"
+SUPABASE_ANNON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsbmR4dHZ2Y3B4dndtYWNmamhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1ODEzMTEsImV4cCI6MjA2MzE1NzMxMX0.TIzsLaQNP7fvZjynSY839QB36EN8kz8LDmbKM1JtYsE"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_ANNON_KEY)
 
